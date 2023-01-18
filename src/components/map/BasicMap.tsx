3b0a4dab5beebf57e27bdff602c52cb03e6e573f
shahnawaz <shahnawaz@eatlbd.com>
2023-01-18 17:07:46 +0600

    satellite, street, hybrid, terrain layers implemented

--- src/components/map/BasicMap.tsx
+++ src/components/map/BasicMap.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 
 export const BasicMap = () => {
@@ -14,11 +14,56 @@ export const BasicMap = () => {
         id="my-map"
         style={{ height: "100vh" }}
       >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="OpenStreetMap">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
         contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+
+          {/* Satellite View */}
+          <LayersControl.BaseLayer name="SatelliteView">
+            <TileLayer
+              url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}" // google satellite, subdomains={['mt0','mt1','mt2','mt3']}
+              // url='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' // openstreetmap satellite, subdomains={['a','b','c']}
+              maxZoom={18}
+              subdomains={["mt0", "mt1", "mt2", "mt3"]}
+            />
+          </LayersControl.BaseLayer>
+          {/* End of Satellite View */}
+
+          {/* Streets View */}
+          <LayersControl.BaseLayer name="StreetView">
+            <TileLayer
+              url="https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // google street, subdomains={['mt0','mt1','mt2','mt3']}
+              maxZoom={18}
+              subdomains={["mt0", "mt1", "mt2", "mt3"]}
+            />
+          </LayersControl.BaseLayer>
+          {/* End of Streets View */}
+
+          {/* Hybrid View */}
+          <LayersControl.BaseLayer name="HybridView">
+            <TileLayer
+              url="https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // google hybrid, subdomains={['mt0','mt1','mt2','mt3']}
+              maxZoom={18}
+              subdomains={["mt0", "mt1", "mt2", "mt3"]}
+            />
+          </LayersControl.BaseLayer>
+          {/* End of Hybrid View */}
+
+          {/* Terrain View */}
+          <LayersControl.BaseLayer name="TerrainView">
+            <TileLayer
+              url="https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // google terrain, subdomains={['mt0','mt1','mt2','mt3']}
+              maxZoom={18}
+              subdomains={["mt0", "mt1", "mt2", "mt3"]}
+            />
+          </LayersControl.BaseLayer>
+          {/* End of Terrain View */}
+        </LayersControl>
       </MapContainer>
     </div>
   );
