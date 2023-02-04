ebfd5dc072ab57e4142ef01913769416d2177fc8
shahnawaz-pabon <s.pabon93@gmail.com>
2023-02-04 23:48:10 +0600

    navbar component added with fontawesome package

---
+++ src/layouts/portal/Navbar.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  Col,
+  Container,
+  Dropdown,
+  Image,
+  ListGroup,
+  Nav,
+  Navbar,
+  Row,
+} from "@themesberg/react-bootstrap";
+
+export default (props: any) => {
+  return (
+    <Navbar variant="dark" expanded className="p-5 pr-30 bg-white"></Navbar>
+  );
+};
